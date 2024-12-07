3640fb827e0da7e39702b108565a9693e16d93c1
zzzRYT <jinjinstar3@gmail.com>
2024-12-07 17:00:54 +0900

    publish(#4):퍼블리싱

--- src/app/(afterAuth)/(contents)/honeyJar/_components/HoneyMoa/honeyMoa.css.ts
+++ src/app/(afterAuth)/(contents)/honeyJar/_components/HoneyMoa/honeyMoa.css.ts
@@ -4,6 +4,11 @@ export const container = style({
   padding: 16,
   border: '1px solid rgba(0, 0, 0, 0.1)',
   borderRadius: 12,
+  transition: 'all 0.2s ease-in-out',
+  cursor: 'pointer',
+  ':hover': {
+    boxShadow: '0 10px 15px -3px rgba(0, 0, 0, 0.2)',
+  },
 });
 
 export const contentWrapper = style({
